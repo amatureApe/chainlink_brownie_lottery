4dd4943dfa8c5cf62226a77ba830ac91773d8ce0
amatureApe <amatureApe@gmail.com>
2022-10-01 00:32:01 -0500

    add test_can_end_lottery test function

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -1,7 +1,11 @@
 from brownie import Lottery, accounts, config, network, exceptions
 from scripts.deploy_lottery import deploy_lottery
 from web3 import Web3
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+    fund_with_link,
+)
 import pytest
 
 
@@ -20,3 +24,25 @@ def test_cant_enter_unless_started():
     lottery = deploy_lottery()
     with pytest.raises(exceptions.VirtualMachineError):
         lottery.enter({"from": get_account(), "value": lottery.getEntranceFee()})
+
+
+def test_can_start_and_enter_lottery():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    assert lottery.players(0) == account
+
+
+def test_can_end_lottery():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    lottery.endLottery({"from": account})
+    assert lottery.lottery_state() == 2
